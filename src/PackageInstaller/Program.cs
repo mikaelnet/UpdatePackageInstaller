69e0915170afed2b6281459ea68279f567e02bf6
Mikael Hogberg <mikael.hogberg@stendahls.se>
2016-05-18 17:38:04 +0200

    Added support for leaving package installer as-is on the server and code cleanup

--- src/PackageInstaller/Program.cs
+++ src/PackageInstaller/Program.cs
@@ -1,268 +1,295 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using NDesk.Options;
 using System.IO;
 using System.Reflection;
-using System.Security.Permissions;
-using System.ServiceModel;
 
 namespace HedgehogDevelopment.PackageInstaller
 {
-    /// <summary>
-    /// Installer command line utility. Uses NDesk.Options to parse the command line. For more information, please see
-    /// http://www.ndesk.org/Options. 
-    /// </summary>
-    class Program
-    {
-        static int verbosity;
-        static string SitecoreConnectorDLL { get; set; }
-        static string SitecoreConnectorASMX { get; set; }
+	/// <summary>
+	/// Installer command line utility. Uses NDesk.Options to parse the command line. For more information, please see
+	/// http://www.ndesk.org/Options. 
+	/// </summary>
+	internal class Program
+	{
+		private static int _verbosity;
+		private static string SitecoreConnectorDll { get; set; }
+		private static string SitecoreConnectorAsmx { get; set; }
 
-        static void Main(string[] args)
-        {
-            #region Declare options and installer variables
-            // Installer variables
-            string packagePath = null;
-            string sitecoreWebURL = null;
-            string sitecoreDeployFolder = null;
-            bool show_help = args.Length == 0;
+		private static void Main(string[] args)
+		{
+			#region Declare options and installer variables
 
-            // Options declaration
-            OptionSet options = new OptionSet() {
-   	            { "p|packagePath=", "The {PACKAGE PATH} is the path to the package. The package must be located in a folder reachable by the web server.\n",
-                    v => packagePath = v },
-                { "u|sitecoreUrl=", "The {SITECORE URL} is the url to the root of the Sitecore server.\n",
-                    v => sitecoreWebURL = v },
-                { "f|sitecoreDeployFolder=", "The {SITECORE DEPLOY FOLDER} is the UNC path to the Sitecore web root.\n",
-                    v => sitecoreDeployFolder = v },
-   	            { "v", "Increase debug message verbosity.\n",
-                    v => { if (v != null) ++verbosity; } },
-   	            { "h|help",  "Show this message and exit.", 
-                    v => show_help = v != null },
-             };
-            #endregion
+			// Installer variables
+			string packagePath = null;
+			string sitecoreWebUrl = null;
+			string sitecoreDeployFolder = null;
+			bool showHelp = args.Length == 0;
+			bool removePackageInstaller = false;
 
-            // Parse options - exit on error
-            List<string> extra;
-            try
-            {
-                extra = options.Parse(args);
-            }
-            catch (OptionException e)
-            {
-                ShowError(e.Message);
-                Environment.Exit(100);
-            }
+			// Options declaration
+			OptionSet options = new OptionSet()
+			{
+				{
+					"p|packagePath=",
+					"The {PACKAGE PATH} is the path to the package. The package must be located in a folder reachable by the web server.\n",
+					v => packagePath = v
+				},
+				{
+					"u|sitecoreUrl=", "The {SITECORE URL} is the url to the root of the Sitecore server.\n",
+					v => sitecoreWebUrl = v
+				},
+				{
+					"f|sitecoreDeployFolder=", "The {SITECORE DEPLOY FOLDER} is the UNC path to the Sitecore web root.\n",
+					v => sitecoreDeployFolder = v
+				},
+				{
+					"v", "Increase debug message verbosity.\n",
+					v => { if (v != null) ++_verbosity; }
+				},
+				{
+					"h|help", "Show this message and exit.",
+					v => showHelp = v != null
+				},
+				{
+					"c|cleanup", "Remove package installer when done",
+					v => removePackageInstaller = v != null
+				}
+			};
 
-            // Display help if one is requested or no parameters are provided
-            if (show_help)
-            {
-                ShowHelp(options);
-                return;
-            }
+			#endregion
 
-            #region Validate and process parameters
-            bool parameterMissing = false;
+			// Parse options - exit on error
+			try
+			{
+				options.Parse(args);
+			}
+			catch (OptionException e)
+			{
+				ShowError(e.Message);
+				Environment.Exit(100);
+			}
 
-            if (string.IsNullOrEmpty(packagePath))
-            {
-                ShowError("Package Path is required.");
+			// Display help if one is requested or no parameters are provided
+			if (showHelp)
+			{
+				ShowHelp(options);
+				return;
+			}
 
-                parameterMissing = true;
-            }
+			#region Validate and process parameters
 
-            if (string.IsNullOrEmpty(sitecoreWebURL))
-            {
-                ShowError("Sitecore Web URL ie required.");
+			bool parameterMissing = false;
 
-                parameterMissing = true;
-            }
+			if (string.IsNullOrEmpty(packagePath))
+			{
+				ShowError("Package Path is required.");
+				parameterMissing = true;
+			}
 
-            if (string.IsNullOrEmpty(sitecoreDeployFolder))
-            {
-                ShowError("Sitecore Deploy folder is required.");
+			if (string.IsNullOrEmpty(sitecoreWebUrl))
+			{
+				ShowError("Sitecore Web URL ie required.");
+				parameterMissing = true;
+			}
 
-                parameterMissing = true;
-            }
+			if (string.IsNullOrEmpty(sitecoreDeployFolder))
+			{
+				ShowError("Sitecore Deploy folder is required.");
+				parameterMissing = true;
+			}
 
-            if (!parameterMissing)
-            {
-                if (Directory.Exists(sitecoreDeployFolder))
-                {
-                    try
-                    {
-                        Debug("Initializing update package installation: {0}", packagePath);
-                        if (sitecoreDeployFolder.LastIndexOf(@"\") != sitecoreDeployFolder.Length - 1)
-                        {
-                            sitecoreDeployFolder = sitecoreDeployFolder + @"\";
-                        }
+			if (parameterMissing) 
+				return;
 
-                        if (sitecoreWebURL.LastIndexOf(@"/") != sitecoreWebURL.Length - 1)
-                        {
-                            sitecoreWebURL = sitecoreWebURL + @"/";
-                        }
+			if (!Directory.Exists(sitecoreDeployFolder))
+			{
+				ShowError(string.Format("Sitecore Deploy Folder {0} not found.", sitecoreDeployFolder));
+				return;
+			}
 
-                        // Install Sitecore connector
-                        if (DeploySitecoreConnector(sitecoreDeployFolder))
-                        {
-                            using (TdsPackageInstaller.TdsPackageInstaller service = new TdsPackageInstaller.TdsPackageInstaller())
-                            {
-                                service.Url = string.Concat(sitecoreWebURL, Properties.Settings.Default.SitecoreConnectorFolder, "/TdsPackageInstaller.asmx");
-                                service.Timeout = 600000;
- 
-                                Debug("Initializing package installation ..");
+			try
+			{
+				Debug("Initializing update package installation: {0}", packagePath);
+				if (sitecoreDeployFolder.LastIndexOf(@"\", StringComparison.Ordinal) != sitecoreDeployFolder.Length - 1)
+				{
+					sitecoreDeployFolder = sitecoreDeployFolder + @"\";
+				}
 
-                                service.InstallPackage(packagePath);
+				if (sitecoreWebUrl.LastIndexOf(@"/", StringComparison.Ordinal) != sitecoreWebUrl.Length - 1)
+				{
+					sitecoreWebUrl = sitecoreWebUrl + @"/";
+				}
 
-                                Debug("Update package installed successfully.");
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("Sitecore connector deployment failed.");
+				// Install Sitecore connector
+				if (DeploySitecoreConnector(sitecoreDeployFolder))
+				{
+					using (TdsPackageInstaller.TdsPackageInstaller service = new TdsPackageInstaller.TdsPackageInstaller())
+					{
+						service.Url = string.Concat(sitecoreWebUrl, Properties.Settings.Default.SitecoreConnectorFolder,
+							"/TdsPackageInstaller.asmx");
+						service.Timeout = 10 * 60 * 1000;
 
-                            Environment.Exit(101);
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine("Exception: {0}({1})\n{2}", ex.Message, ex.GetType().Name, ex.StackTrace);
+						Debug("Initializing package installation ..");
 
-                        if (ex.InnerException != null)
-                        {
-                            Console.WriteLine("\n\nInnerException: {0}({1})\n{2}", ex.InnerException.Message, ex.InnerException.GetType().Name, ex.InnerException.StackTrace);
-                        }
+						service.InstallPackage(packagePath);
 
-                        Environment.Exit(102);
-                    }
-                    finally
-                    {
-                        // Remove Sitecore connection
-                        RemoveSitecoreConnector();
-                    }
-                }
-                else
-                {
-                    ShowError(string.Format("Sitecore Deploy Folder {0} not found.", sitecoreDeployFolder));
-                }
-            }
+						Debug("Update package installed successfully.");
+					}
+				}
+				else
+				{
+					Console.WriteLine("Sitecore connector deployment failed.");
 
-            #endregion
-        }
+					Environment.Exit(101);
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Exception: {0}({1})\n{2}", ex.Message, ex.GetType().Name, ex.StackTrace);
 
-        /// <summary>
-        /// Displays the help message
-        /// </summary>
-        /// <param name="opts"></param>
-        static void ShowHelp(OptionSet opts)
-        {
-            Console.WriteLine("Usage: packageinstaller [OPTIONS]");
-            Console.WriteLine("Installs a sitecore package.");
-            Console.WriteLine();
-            Console.WriteLine("Example:");
-            Console.WriteLine(@"-v -sitecoreUrl ""http://mysite.com/"" -sitecoreDeployFolder ""C:\inetpub\wwwroot\mysite\Website"" -packagePath ""C:\Package1.update""");
-            Console.WriteLine();
-            Console.WriteLine("Options:");
+				if (ex.InnerException != null)
+				{
+					Console.WriteLine("\n\nInnerException: {0}({1})\n{2}", ex.InnerException.Message,
+						ex.InnerException.GetType().Name, ex.InnerException.StackTrace);
+				}
 
-            opts.WriteOptionDescriptions(Console.Out);
-        }
+				Environment.Exit(102);
+			}
+			finally
+			{
+				if (removePackageInstaller)
+				{
+					// Remove Sitecore connection
+					RemoveSitecoreConnector();
+				}
+			}
 
-        /// <summary>
-        /// Displays an error message
-        /// </summary>
-        /// <param name="message"></param>
-        static void ShowError(string message)
-        {
-            Console.Write("Error: ");
-            Console.WriteLine(message);
-            Console.WriteLine("Try `packageinstaller --help' for more information.");
-        }
+			#endregion
+		}
 
-        /// <summary>
-        /// Deploys the 
-        /// </summary>
-        /// <param name="sitecoreDeployFolder"></param>
-        /// <returns></returns>
-        static bool DeploySitecoreConnector(string sitecoreDeployFolder)
-        {
-            Debug("Initializing Sitecore connector ...");
+		/// <summary>
+		/// Displays the help message
+		/// </summary>
+		/// <param name="opts"></param>
+		private static void ShowHelp(OptionSet opts)
+		{
+			Console.WriteLine("Usage: packageinstaller [OPTIONS]");
+			Console.WriteLine("Installs a sitecore package.");
+			Console.WriteLine();
+			Console.WriteLine("Example:");
+			Console.WriteLine(
+				@"-v -sitecoreUrl ""http://mysite.com/"" -sitecoreDeployFolder ""C:\inetpub\wwwroot\mysite\Website"" -packagePath ""C:\Package1.update""");
+			Console.WriteLine();
+			Console.WriteLine("Options:");
 
-            string sourceFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            FileInfo serviceLibrary = new FileInfo(sourceFolder + @"\HedgehogDevelopment.TDS.PackageInstallerService.dll");
-            FileInfo serviceFile = new FileInfo(sourceFolder + @"\Includes\TdsPackageInstaller.asmx");
+			opts.WriteOptionDescriptions(Console.Out);
+		}
 
-            if (!serviceLibrary.Exists)
-            {
-                ShowError("Cannot find file " + serviceLibrary);
+		/// <summary>
+		/// Displays an error message
+		/// </summary>
+		/// <param name="message"></param>
+		private static void ShowError(string message)
+		{
+			Console.Write("Error: ");
+			Console.WriteLine(message);
+			Console.WriteLine("Try `packageinstaller --help' for more information.");
+		}
 
-                return false;
-            }
+		/// <summary>
+		/// Deploys the 
+		/// </summary>
+		/// <param name="sitecoreDeployFolder"></param>
+		/// <returns></returns>
+		private static bool DeploySitecoreConnector(string sitecoreDeployFolder)
+		{
+			Debug("Initializing Sitecore connector ...");
 
-            if (!serviceFile.Exists)
-            {
-                ShowError("Cannot find file " + serviceFile);
+			string sourceFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			FileInfo serviceLibrary = new FileInfo(sourceFolder + @"\HedgehogDevelopment.TDS.PackageInstallerService.dll");
+			FileInfo serviceFile = new FileInfo(sourceFolder + @"\Includes\TdsPackageInstaller.asmx");
 
-                return false;
-            }
+			if (!serviceLibrary.Exists)
+			{
+				ShowError("Cannot find file " + serviceLibrary);
 
-            if (!Directory.Exists(sitecoreDeployFolder + Properties.Settings.Default.SitecoreConnectorFolder))
-            {
-                Directory.CreateDirectory(sitecoreDeployFolder + Properties.Settings.Default.SitecoreConnectorFolder);
-            }
+				return false;
+			}
 
-            SitecoreConnectorDLL = sitecoreDeployFolder + @"bin\" + serviceLibrary.Name;
-            SitecoreConnectorASMX = sitecoreDeployFolder + Properties.Settings.Default.SitecoreConnectorFolder + @"\" + serviceFile.Name;
+			if (!serviceFile.Exists)
+			{
+				ShowError("Cannot find file " + serviceFile);
 
-            if (File.Exists(SitecoreConnectorDLL))
-            {
-                File.SetAttributes(SitecoreConnectorDLL, FileAttributes.Normal);
-                File.SetAttributes(SitecoreConnectorASMX, FileAttributes.Normal);
-            }
+				return false;
+			}
 
-            if (File.Exists(SitecoreConnectorASMX))
-            {
-                File.SetAttributes(SitecoreConnectorASMX, FileAttributes.Normal);
-            }
+			if (!Directory.Exists(sitecoreDeployFolder + Properties.Settings.Default.SitecoreConnectorFolder))
+			{
+				Directory.CreateDirectory(sitecoreDeployFolder + Properties.Settings.Default.SitecoreConnectorFolder);
+			}
 
-            File.Copy(serviceLibrary.FullName, SitecoreConnectorDLL, true);
-            File.Copy(serviceFile.FullName, SitecoreConnectorASMX, true);
+			SitecoreConnectorDll = sitecoreDeployFolder + @"bin\" + serviceLibrary.Name;
+			SitecoreConnectorAsmx = sitecoreDeployFolder + Properties.Settings.Default.SitecoreConnectorFolder + @"\" +
+			                        serviceFile.Name;
 
-            Debug("Sitecore connector deployed successfully.");
+			CopyIfChanged(serviceLibrary, new FileInfo(SitecoreConnectorDll));
+			CopyIfChanged(serviceFile, new FileInfo(SitecoreConnectorAsmx));
 
-            return true;
-        }
+			Debug("Sitecore connector deployed successfully.");
 
-        /// <summary>
-        /// Removes the sitecore connector from the site
-        /// </summary>
-        static void RemoveSitecoreConnector()
-        {
-            if (!string.IsNullOrEmpty(SitecoreConnectorDLL) && !string.IsNullOrEmpty(SitecoreConnectorASMX))
-            {
-                File.SetAttributes(SitecoreConnectorDLL, FileAttributes.Normal);
-                File.SetAttributes(SitecoreConnectorASMX, FileAttributes.Normal);
+			return true;
+		}
 
-                File.Delete(SitecoreConnectorDLL);
-                File.Delete(SitecoreConnectorASMX);
+		private static void CopyIfChanged(FileInfo source, FileInfo target)
+		{
+			if (!source.Exists)
+				return;
 
-                Debug("Sitecore connector removed successfully.");
-            }
-        }
+			if (!target.Exists)
+			{
+				File.Copy(source.FullName, target.FullName);
+				File.SetAttributes(target.FullName, FileAttributes.Normal);
+				return;
+			}
 
-        /// <summary>
-        /// Writes a debug message to the console
-        /// </summary>
-        /// <param name="format"></param>
-        /// <param name="args"></param>
-        static void Debug(string format, params object[] args)
-        {
-            if (verbosity > 0)
-            {
-                Console.Write(string.Format("[{0}] ", DateTime.Now.ToString("hh:mm:ss")));
-                Console.WriteLine(format, args);
-            }
-        }
-    }
+			if (source.Length != target.Length || source.LastWriteTime > target.LastWriteTime)
+			{
+				File.Copy(source.FullName, target.FullName);
+				File.SetAttributes(target.FullName, FileAttributes.Normal);
+			}
+		}
+
+		/// <summary>
+		/// Removes the sitecore connector from the site
+		/// </summary>
+		private static void RemoveSitecoreConnector()
+		{
+			if (!string.IsNullOrEmpty(SitecoreConnectorDll) && !string.IsNullOrEmpty(SitecoreConnectorAsmx))
+			{
+				File.SetAttributes(SitecoreConnectorDll, FileAttributes.Normal);
+				File.SetAttributes(SitecoreConnectorAsmx, FileAttributes.Normal);
+
+				File.Delete(SitecoreConnectorDll);
+				File.Delete(SitecoreConnectorAsmx);
+
+				Debug("Sitecore connector removed successfully.");
+			}
+		}
+
+		/// <summary>
+		/// Writes a debug message to the console
+		/// </summary>
+		/// <param name="format"></param>
+		/// <param name="args"></param>
+		private static void Debug(string format, params object[] args)
+		{
+			if (_verbosity > 0)
+			{
+				Console.Write("[{0}] ", DateTime.Now.ToString("hh:mm:ss"));
+				Console.WriteLine(format, args);
+			}
+		}
+	}
 }
